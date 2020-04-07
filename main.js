4394c49ec2024587cba4c7c7d110a5263c5509e7
FichoZee <filip.zafran@gmail.com>
2020-04-07 17:41:28 +0200

    update text, add update add picture

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-main.js;
+const updateText = () => {
+  const userText = document.getElementById("userText");
+  const memeText = document.getElementById("memeText");
+
+  memeText.innerText = userText.value;
+};
